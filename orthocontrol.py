f1e4b5fc7087d02d6501d5b444bcf1c69919c7cb
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2024-04-13 00:17:10 +0100

    Clean everything up and wire in volume control

--- orthocontrol.py
+++ orthocontrol.py
@@ -1,186 +1,158 @@
-import subprocess
 import sys
+import subprocess
+import getopt
 import rtmidi
 import time
-import getopt
-from Quartz.CoreGraphics import CGEventPost
-from Quartz.CoreGraphics import kCGHIDEventTap
-from Quartz.CoreGraphics import kCGEventFlagMaskShift
-from Quartz.CoreGraphics import kCGEventFlagMaskAlternate
+from functools import wraps
+from threading import Timer
+from Quartz.CoreGraphics import CGEventPost, kCGHIDEventTap
 from AppKit import NSEvent
 from CoreMIDI import MIDIRestart
+import logging
 
-options, _ = getopt.getopt(sys.argv[1:], [], ["midi-name=", "midi-restart", "midi-restart-interval=", "midi-sysex", "midi-notifications"])
-options = dict(options)
-
-if not ("--midi-name" in options):
-    print("Missing --midi-name argument")
-    sys.exit(1)
-
-port_name = options["--midi-name"]
-
-if "--midi-restart" in options:
-    restart_enable = True
-else:
-    restart_enable = False
-
-if "--midi-restart-interval" in options:
-    restart_interval = float(options["--midi-restart-interval"])
-else:
-    restart_interval = 1.0
-
-if "--midi-sysex" in options:
-    sysex_enable = True
-else:
-    sysex_enable = False
-
-if "--midi-notifications" in options:    
-    notification_enable = True
-else:
-    notification_enable = False
-
-print("Applied arguments:")
-print(f"  MIDI port name:        {port_name}'")
-print(f"  MIDI restart:          {restart_enable}")
-print(f"  MIDI restart interval: {restart_interval}")
-print(f"  MIDI sysex:            {sysex_enable}")
-print(f"  MIDI notifications:    {notification_enable}")
-
-# bits taken from:
-# https://github.com/boppreh/keyboard/blob/master/keyboard/_darwinkeyboard.py
-
-CODE_VOLUME_UP = 0
-CODE_VOLUME_DOWN = 1
+# Constants
 CODE_PLAY = 16
 
-FLAG_DEFAULT = 0
-FLAG_ALT_SHIFT = kCGEventFlagMaskShift + kCGEventFlagMaskAlternate
+def setup_logging(level='info'):
+    level_dict = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+    numeric_level = level_dict.get(level.lower(), logging.INFO)  # Default to INFO if level is not recognized
+    logging.basicConfig(
+        level=numeric_level,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+def process_command_line_args():
+    try:
+        options, _ = getopt.getopt(
+            sys.argv[1:],
+            '',
+            ["midi-name=", "midi-restart", "midi-restart-interval=", "midi-sysex", "midi-notifications", "log-level="]
+        )
+        options = dict(options)
+        if "--midi-name" not in options:
+            logging.error("Missing --midi-name argument")
+            sys.exit(1)
+        if "--log-level" in options:
+            setup_logging(options["--log-level"])
+        else:
+            setup_logging()  # Default setup if no log level specified
+        logging.info("Command line arguments processed successfully.")
+        return options
+    except getopt.GetoptError as e:
+        logging.error(f"Command line error: {e}")
+        sys.exit(1)
+
+def throttle_debounce(throttle_ms, debounce_ms):
+    throttle_interval = throttle_ms / 1000.0
+    debounce_interval = debounce_ms / 1000.0
+
+    def decorator(func):
+        last_call = [0]
+        debounce_timer = [None]
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            now = time.time()
+            if debounce_timer[0] is not None:
+                debounce_timer[0].cancel()
+
+            if now - last_call[0] > throttle_interval:
+                last_call[0] = now
+                func(*args, **kwargs)
+            else:
+                def call_it():
+                    func(*args, **kwargs)
+                    last_call[0] = time.time()
+                debounce_timer[0] = Timer(debounce_interval, call_it)
+                debounce_timer[0].start()
+
+        return wrapper
+    return decorator
+
+def set_application_volume(app_name, volume):
+    if not 0 <= volume <= 100:
+        raise ValueError("Volume must be between 0 and 100.")
+    script = f"""
+    tell application "System Events"
+        if exists (application process "{app_name}") then
+            tell application "{app_name}"
+                set sound volume to {volume}
+            end tell
+        end if
+    end tell
+    """
+    try:
+        subprocess.run(["osascript", "-e", script], check=True)
+        logging.debug(f"{app_name} volume set to {volume}%.")
+    except subprocess.CalledProcessError:
+        logging.error(f"Failed to set {app_name} volume.")
+
+@throttle_debounce(throttle_ms=250, debounce_ms=100)
+def set_volume(volume):
+    logging.debug(f"Setting volume to {volume}%.")
+    set_application_volume("Music", volume)
+    set_application_volume("Spotify", volume)
 
 def tap(code, flags=0):
     event = NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
-        14, # type
-        (0, 0), # location
-        0xa00 + flags, # flags
-        0, # timestamp
-        0, # window
-        0, # ctx
-        8, # subtype
-        (code << 16) | (0xa << 8), # data1
-        -1 # data2
-    )
-    CGEventPost(kCGHIDEventTap, event.CGEvent())
-    event = NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
-        14, # type
-        (0, 0), # location
-        0xb00 + flags, # flags
-        0, # timestamp
-        0, # window
-        0, # ctx 
-        8, # subtype
-        (code << 16) | (0xb << 8), # data1
-        -1 # data2
+        14, (0, 0), 0xA00 + flags, 0, 0, 0, 8, (code << 16) | (0xA << 8), -1
     )
     CGEventPost(kCGHIDEventTap, event.CGEvent())
 
-def volume(up, quadruple=False):
-    code = CODE_VOLUME_UP if up else CODE_VOLUME_DOWN
-    flags = FLAG_DEFAULT if quadruple else FLAG_ALT_SHIFT
-    tap(code, flags)
-
-def play():
+def toggle_play_pause():
+    logging.debug("Toggling play/pause")
     tap(CODE_PLAY)
 
-scroll_last = None
-click_last = 0
+def midi_callback(message, time_stamp):
+    message_type, note, velocity = message[0]
 
-def callback(message, delta):
-    global scroll_last
-    global click_last
-    if message[0][0] == 176:
-        if message[0][2] == 1:
-            volume(True)
-        elif message[0][2] == 127:
-            volume(False)
-    elif message[0][0] == 144 or message[0][0] == 128:
-        click_now = message[0][2] == 100
-        if click_now != click_last:
-            if click_now:
-                play()
-            click_last = click_now
+    if message_type == 176:
+        volume_percentage = int((velocity / 127.0) * 100)
+        set_volume(volume_percentage)
+        logging.debug(f"Volume adjusted to {volume_percentage}% based on MIDI control.")
 
-midi_in = rtmidi.MidiIn()
-midi_out = rtmidi.MidiOut()
+    elif message_type == 144:
+        toggle_play_pause()
+        logging.debug(f"Play/Pause toggled based on MIDI note {note}.")
 
-if restart_enable:
-    print("Restarting MIDI server")
-    MIDIRestart()
+def main():
+    options = process_command_line_args()
+    midi_in = rtmidi.MidiIn()
+    midi_out = rtmidi.MidiOut()
 
-notification_command = '''
-on run argv
-  display notification (item 2 of argv) with title (item 1 of argv)
-end run
-'''
+    port_name = options["--midi-name"]
+    restart_interval = float(options.get("--midi-restart-interval", 1.0))
+    sysex_enable = "--midi-sysex" in options
+    restart_enable = "--midi-restart" in options
 
-def notification(notification_message):
-    print(notification_message)
-    if notification_enable:
-        subprocess.call(['osascript', '-e', notification_command, 'OrthoControl', notification_message])
+    while True:
+        ports_in = midi_in.get_ports()
+        ports_out = midi_out.get_ports()
+        if port_name in ports_in and port_name in ports_out:
+            try:
+                with midi_in.open_port(ports_in.index(port_name)) as port_in, \
+                     midi_out.open_port(ports_out.index(port_name)) as port_out:
+                    if sysex_enable:
+                        midi_out.send_message([0xF0, 0x00, 0x20, 0x76, 0x02, 0x00, 0x02, 0x00, 0xF7])
+                    midi_in.set_callback(midi_callback)
+                    while port_name in midi_in.get_ports() and port_name in midi_out.get_ports():
+                        time.sleep(restart_interval)
+                    midi_in.cancel_callback()
+            except Exception as e:
+                logging.error(f"Error with MIDI port {port_name}: {str(e)}")
+        else:
+            logging.info(f"Port unavailable: '{port_name}'")
 
-"""
-OR1 SYSEX SPEC
-strt | TE       OR1 | cmd  addr values | end
------|--------------|------------------|----
-F0   | 00 20 76 02  | xx   xx   xx ... | F7
+        if restart_enable:
+            MIDIRestart()
+        time.sleep(restart_interval)
 
-cmd
-00      write
-01      read
-
-addr    setting                            default     note
-00      midi channel                       0
-01      midi cc                            1
-02      midi cc abs (0 or 1)               1           set to 1 to enable absolute mode
-03      midi note                          60
-04      midi velocity                      100
-05      disable hid                        0           set to 1 to disable hid - will restart remote
-06      set cc absolute value              63          write 63 to reset
-
-values
-write   list of values for consecutive addresses after addr
-read    number of consecutive addresses to read after addr (0 = read all)
-        - response msg can be used to write those values back
-
-EXAMPLES
-F0 00 20 76 02 00 01 0e F7     set send cc 14
-F0 00 20 76 02 01 00 00 F7     read all
-"""
-
-while True:
-    ports_in = midi_in.get_ports()
-    ports_out = midi_out.get_ports()
-    if port_name in ports_in and port_name in ports_out:
-        try:
-            with midi_in.open_port(ports_in.index(port_name)) as port_in, midi_out.open_port(ports_out.index(port_name)) as port_out:
-                notification(f"Port opened successfully: '{port_name}'")
-                if sysex_enable:
-                    print(f"Sending sysex to enable relative mode")
-                    midi_out.send_message([0xF0, 0x00, 0x20, 0x76, 0x02, 0x00, 0x02, 0x00, 0xF7])
-                midi_in.set_callback(callback)
-                while True:
-                    time.sleep(restart_interval)
-                    ports_in = midi_in.get_ports()
-                    ports_out = midi_out.get_ports()
-                    if not port_name in ports_in or not port_name in ports_out:
-                        break
-                midi_in.cancel_callback()
-            notification(f"Port closed: '{port_name}'")
-        except Exception:
-            notification(f"Port failed to open or encountered an error: '{port_name}'")         
-    else:
-        notification(f"Port unavaliable: '{port_name}'")
-        print(f"Currently avaliable ports (in):  {ports_in}")
-        print(f"Currently avaliable ports (out): {ports_out}")
-    if restart_enable:
-        notification("Restarting MIDI server")
-        MIDIRestart()
-    time.sleep(restart_interval)
+if __name__ == "__main__":
+    main()
